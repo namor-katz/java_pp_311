5d75d4b269f2815b108f49cbbc8a299cc1daefb4
RomanK <namor925@gmail.com>
2020-06-02 22:21:05 +0700

    add rest api edit method

--- src/main/java/com/katzendorn/demo/controller/UserRestController.java
+++ src/main/java/com/katzendorn/demo/controller/UserRestController.java
@@ -51,21 +51,20 @@ public class UserRestController {
     })
     @DeleteMapping(value = "user/delete/{id}")  //можно юзать делете или гет, и соотв. контроллер. лучше делете, видимо.
     //это вообще нужно? все эти коде, ибо отдаёт оно почему-то 200, а не 204
-    public ResponseEntity<User> deleteUser(@PathVariable String id) {
-        String id0 = id.replace("id", "");
-        Long id1 = Long.parseLong(id0);
-        User user = userService.findUserById(id1);
-        if(user == null) {
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+//        String id0 = id.replace("id", "");
+//        Long id1 = Long.parseLong(id0);
+        User user = userService.findUserById(id);
+        if(user.getUsername() == null) {
             System.out.println("don't find user");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        userService.deleteUser(id1);
+        userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @ApiOperation(value = "create new User", code = 201, response = User.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Succesfully created user")
-    })
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Succesfully created user")})
     @PostMapping(value = "user/new", consumes = {"application/json"})
     public ResponseEntity<User> createNewUser(@RequestBody User user) {
         System.out.println("но я и есть юзер " + user.getUsername());
@@ -73,4 +72,25 @@ public class UserRestController {
         userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @PutMapping("user/update/{id}")
+    @ApiOperation(value = "Update exixting user", code = 202, response = User.class)
+    @ApiResponses(value = {@ApiResponse(code = 202, message = "Update accepted")})
+    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User user) {
+        Long id0 = Long.parseLong(id);
+        User userOfDb = userService.findUserById(id0);
+        Set<Role> roles  = userOfDb.getRoles();
+        if(userOfDb.getUsername() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        //!! требуется логика проверок, либо забить и юзать просто поля из формы.
+        user.setId(id0);
+        user.setRoles(roles);
+
+        userService.updateUser(user);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+
 }
